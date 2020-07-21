0971c2e2e2fad09f66f1dff05619f972ef8f926f
Yana95 <79191411745@yandex.ru>
2020-07-21 17:31:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import json
+
+with open("newsafr.json", encoding = "utf-8") as f:
+  json_data = json.load(f)
+#print(json_data)
+news_list = json_data["rss"]["channel"]["items"]
+#print(news_list)
+
+
+for news in news_list:
+  news2 = news["description"]
+  #print(news2)
+  n_list = news2.split(" ")
+  #print(n_list)
+
+word_list = []
+
+for w in n_list:
+  if len(w) >= 6:
+    word_list.append(w)
+
+#print(word_list)
+from collections import Counter
+long_words = Counter(word_list).most_common(10)
+for k in long_words:
+  print(k) 
+
+
+
+  
+    
+
+
+
+
+
+    
+
+
+
+
+ 
+
+
