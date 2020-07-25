91c16a719f6f20ffa65b4291e12d4bec539cd571
Yana95 <79191411745@yandex.ru>
2020-07-25 16:44:54 +0000

    all

--- main.py
+++ main.py
@@ -6,20 +6,18 @@ with open("newsafr.json", encoding = "utf-8") as f:
 news_list = json_data["rss"]["channel"]["items"]
 #print(news_list)
 
+word_list = []
 
 for news in news_list:
   news2 = news["description"]
   #print(news2)
   n_list = news2.split(" ")
   #print(n_list)
+  for w in n_list:
+    if len(w) >= 6:
+      word_list.append(w)
+      #print(word_list)
 
-word_list = []
-
-for w in n_list:
-  if len(w) >= 6:
-    word_list.append(w)
-
-#print(word_list)
 from collections import Counter
 long_words = Counter(word_list).most_common(10)
 for k in long_words:
